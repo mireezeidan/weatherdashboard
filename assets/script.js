371e80e01c5f4c97e3475d2dd926fc6cfa34988b
Miree Zeidan <mireezeidan@gmail.com>
2022-11-24 11:59:50 -0600

    added css to header and searchDiv

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 // places youre going to attach data to
 let searchBtn = document.querySelector("#searchBtn");
 let searchDiv = document.querySelector("#searchDiv");
-let recentSearchDiv = document.querySelector("#recentSearchDiv");
+let recentSearchDiv = document.querySelector("#recentSearches");
 let cityDiv = document.querySelector("#cityDiv");
 let searchText = document.querySelector("#searchText");
 let forecastCard = document.querySelector("#forecastCard");
@@ -29,6 +29,8 @@ function citySearch() {
 
   cities.push(city);
   localStorage.setItem("cities", JSON.stringify(cities));
+  cityDiv.classList.remove("hidden");
+  forecastCard.classList.remove("hidden");
 
   let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=517f19dc586407c39701b016a6edf914&units=imperial`;
 
@@ -39,24 +41,42 @@ function citySearch() {
     .then(function (data) {
       console.log(data);
 
-      currentTemp.innerHTML = data.list[0].main.temp;
-      currentHumi.innerHTML = data.list[0].main.humidity;
-      currentWind.innerHTML = data.list[0].wind.speed;
+      currentTemp.innerHTML = `Temp: ${data.list[0].main.temp}`;
+      currentHumi.innerHTML = `Humidity ${data.list[0].main.humidity}`;
+      currentWind.innerHTML = `Wind Speed: ${data.list[0].wind.speed}`;
 
       // 5 day forecast
       data.list.forEach((day) => {
         let midnight = day.dt_txt.split(" ")[1];
         if (midnight === "00:00:00") {
           let dayCard = document.createElement("div");
-          dayCard.innerHTML += `<div>${day.dt_txt}</div>`;
-          dayCard.innerHTML += `<div>${day.main.temp}</div>`;
-          dayCard.innerHTML += `<div>${day.main.humidity}</div>`;
+          dayCard.innerHTML += `<h3>${day.dt_txt}</h3>`;
+          // dayCard.innerHTML += `<img>${day.weather.icon}</img>`;
+          dayCard.innerHTML += `<p>Temp: ${day.main.temp}</p>`;
+          dayCard.innerHTML += `<p>Humidity: ${day.main.humidity}</p>`;
 
           forecastCard.append(dayCard);
         }
       });
     });
 }
+
+// function prevCity() {
+//   let city = cityBtn.innerHTML;
+//   let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=517f19dc586407c39701b016a6edf914&units=imperial`;
+
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data2) {
+//       console.log(data2);
+
+//       currentTemp.innerHTML = `Temp: ${data2.list[0].main.temp}`;
+//       currentHumi.innerHTML = `Humidity ${data2.list[0].main.humidity}`;
+//       currentWind.innerHTML = `Wind Speed: ${data2.list[0].wind.speed}`;
+//     });
+// }
 
 // function calls
 // event listeners
@@ -65,3 +85,4 @@ init();
 // search button event listener
 searchBtn.addEventListener("click", citySearch);
 // click on past search buttons, does same as clicking on search button
+// cityBtn.addEventListener("click", prevCity);
